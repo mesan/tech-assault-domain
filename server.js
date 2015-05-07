2cc019f1898c71b3e1c64b75ca6a372c080104b5
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-05-07 21:30:59 +0200

    Add root endpoint and add SSL.

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
+import fs from 'fs';
 import Hapi from 'hapi';
 import boardEndpoints from './board/boardEndpoints';
 import cardEndpoints from './card/cardEndpoints';
 
 let server = new Hapi.Server();
 
-server.connection({ port: process.env.PORT || 3000 });
+server.connection({
+    port: process.env.PORT || 3001,
+    tls: {
+        key: fs.readFileSync('./ssl/server.key'),
+        cert: fs.readFileSync('./ssl/server.crt'),
+        ca: fs.readFileSync('./ssl/ca.crt'),
+        requestCert: true,
+        rejectUnauthorized: false
+    }
+});
 
 server.start(() => {
     console.log('Server running at:', server.info.uri);
 
     boardEndpoints(server);
     cardEndpoints(server);
+
+    server.route({
+        method: ['GET'],
+        path: '/',
+        handler: function (request, reply) {
+            return reply('hello');
+        }
+    });
 });
