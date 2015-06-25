bcdbe863404285d7fb5cc876af85a63d80435e01
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-06-25 22:15:35 +0200

    Prefix env variable for mongolab uri.

--- card/controllers/getBaseCardController.js
+++ card/controllers/getBaseCardController.js
@@ -2,8 +2,8 @@ import Card from '../domain/Card';
 import {MongoClient, ObjectID} from 'mongodb';
 
 export default function getBaseCardController(request, reply) {
-    if (process.env.MONGOLAB_URI) {
-        MongoClient.connect(process.env.MONGOLAB_URI, (err, db) => {
+    if (process.env.TECH_DOMAIN_MONGOLAB_URI) {
+        MongoClient.connect(process.env.TECH_DOMAIN_MONGOLAB_URI, (err, db) => {
             var collection = db.collection('baseCardCollection');
             collection.find({ _id: ObjectID(request.params.baseCardId) }).toArray((err, docs) => {
                 reply(docs);
