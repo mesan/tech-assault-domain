ee0d55917293347ec534c60acb642a1dae197186
Jan Eirik <janeirik@hallstensen.net>
2015-08-30 12:14:41 +0200

    Prototype code for finding enemy cards and triggering battles based on player card position on the board

--- match/services/battleService.js
+++ match/services/battleService.js
@@ -1,10 +1,20 @@
 import engine from '../../engine/gameEngine';
 import gameBoard from './gameBoardService';
 
+var isOppsingCardPointingToPlayer = function (arrowPositionPointingToOppsosingCard, opposingCard) {
+    let position = arrowPositionPointingToOppsosingCard + 4;
+
+    if (position > 7) {
+        position = position - 7;
+    }
+
+    return opposingCard.arrows[position] === 1;
+}
+
 export default {
     performBattles(board, cards, placedCard, cardPosition) {
 
-        let gameboard = gameBoard(board, cards);
+        let battles = gameBoard(board, cards).findBattlingCards(placedCard, cardPosition);
 
      /*   if (typeof gameboard.toCoords(16) === 'undefined') {
             console.log("coords is out of bounds!");
@@ -12,7 +22,24 @@ export default {
         if (typeof gameboard.toIndex([5,3]) === "undefined") {
             console.log("index is out of bounds");
         }*/
-        gameboard.findBattlingCards(placedCard, cardPosition);
+        //let battles = gameboard.findBattlingCards(placedCard, cardPosition);
+
+        battles.forEach(function(battle) {
+            let opposingCard = battle.card;
+            let arrowPositionPointingToOppsosingCard = battle.arrowIndex;
+
+            // Is enemy card pointing to the player card
+            if (isOppsingCardPointingToPlayer(arrowPositionPointingToOppsosingCard, opposingCard)) {
+                console.log("BATTLE!!!");
+                let outcome = engine(placedCard, battle.card);
+                console.log("WINNER: " + outcome.winner);
+            }
+            else {
+                console.log("TAKES IT!");
+            }
+
+        });
+
 
         return {
             events: []
