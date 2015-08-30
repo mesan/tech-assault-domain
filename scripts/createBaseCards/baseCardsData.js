6114de7d89c1c6f9cc737e927c3e23a4a2af14ed
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-08-30 16:58:22 +0200

    Add image to base cards.

--- scripts/createBaseCards/baseCardsData.js
+++ scripts/createBaseCards/baseCardsData.js
@@ -1,84 +1,98 @@
 export default [
     {
         name: 'NodeJS',
+        image: 'assets/mesan.png',
         attack: 3,
         defense: 2,
         arrows: [0, 1, 0, 0, 0, 0, 1, 1]
     },
     {
         name: 'Java',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 1,
         arrows: [1, 0, 0, 1, 0, 0, 1, 0]
     },
     {
         name: 'PHP',
+        image: 'assets/mesan.png',
         attack: 1,
         defense: 2,
         arrows: [0, 0, 1, 0, 1, 1, 0, 0]
     },
     {
         name: 'Ruby',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 2,
         arrows: [1, 1, 0, 0, 0, 0, 0, 0]
     },
     {
         name: 'Ruby on Rails',
+        image: 'assets/mesan.png',
         attack: 3,
         defense: 2,
         arrows: [0, 0, 0, 1, 1, 0, 0, 1]
     },
     {
         name: 'Spring',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 2,
         arrows: [0, 0, 0, 0, 1, 0, 0, 1]
     },
     {
         name: 'C#',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 1,
         arrows: [1, 0, 1, 1, 0, 1, 0, 0]
     },
     {
         name: 'NPM',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 2,
         arrows: [1, 0, 0, 1, 0, 1, 0, 0]
     },
     {
         name: 'Python',
+        image: 'assets/mesan.png',
         attack: 1,
         defense: 2,
         arrows: [1, 0, 1, 1, 0, 1, 0, 0]
     },
     {
         name: 'Elixir',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 2,
         arrows: [1, 1, 1, 1, 0, 1, 0, 0]
     },
     {
         name: 'Angular',
+        image: 'assets/mesan.png',
         attack: 3,
         defense: 1,
         arrows: [0, 0, 0, 1, 0, 0, 0, 0]
     },
     {
         name: 'React',
+        image: 'assets/mesan.png',
         attack: 4,
         defense: 2,
         arrows: [1, 0, 1, 1, 0, 1, 0, 0]
     },
     {
         name: 'MVC',
+        image: 'assets/mesan.png',
         attack: 1,
         defense: 1,
         arrows: [0, 0, 1, 0, 0, 1, 0, 1]
     },
     {
         name: 'Flux',
+        image: 'assets/mesan.png',
         attack: 2,
         defense: 2,
         arrows: [0, 0, 0, 1, 1, 0, 0, 1]
