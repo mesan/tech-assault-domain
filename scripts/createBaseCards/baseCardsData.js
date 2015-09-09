d134d5a3203201925af108ef59759afca6926d1b
Arild Wanvik Tvergrov <arildt@mesan.no>
2015-09-09 21:20:32 +0200

    Add more card types.

--- scripts/createBaseCards/baseCardsData.js
+++ scripts/createBaseCards/baseCardsData.js
@@ -100,14 +100,14 @@ export default [
     {
         name: 'Apple',
         image: 'assets/apple.png',
-        attack: 2,
-        defense: 1,
+        attack: 5,
+        defense: 2,
         arrows: [0, 0, 0, 0, 0, 0, 0, 0]
     },
     {
         name: 'Linux',
         image: 'assets/linux.png',
-        attack: 2,
+        attack: 3,
         defense: 1,
         arrows: [0, 1, 0, 1, 0, 1, 1, 0]
     },
@@ -117,5 +117,138 @@ export default [
         attack: 0,
         defense: 4,
         arrows: [0, 0, 0, 1, 0, 1, 1, 0]
+    },
+    {
+        name: 'Google Chrome',
+        image: 'assets/chrome.png',
+        attack: 4,
+        defense: 1,
+        arrows: [0, 1, 1, 1, 0, 0, 1, 0]
+    },
+    {
+        name: 'Duke',
+        image: 'assets/duke.png',
+        attack: 1,
+        defense: 1,
+        arrows: [0, 1, 0, 0, 1, 1, 0, 0]
+    },
+    {
+        name: 'Eclipse',
+        image: 'assets/eclipse.png',
+        attack: 0,
+        defense: 0,
+        arrows: [0, 1, 1, 1, 0, 1, 1, 1]
+    },
+    {
+        name: 'Microsoft Edge',
+        image: 'assets/edge.png',
+        attack: 2,
+        defense: 1,
+        arrows: [0, 1, 0, 1, 0, 1, 0, 1]
+    },
+    {
+        name: 'Ember',
+        image: 'assets/ember.png',
+        attack: 1,
+        defense: 2,
+        arrows: [1, 0, 1, 1, 0, 0, 0, 1]
+    },
+    {
+        name: 'Firefox',
+        image: 'assets/firefox.png',
+        attack: 3,
+        defense: 3,
+        arrows: [1, 1, 0, 0, 0, 0, 0, 1]
+    },
+    {
+        name: 'Gulp',
+        image: 'assets/gulp.png',
+        attack: 2,
+        defense: 1,
+        arrows: [0, 0, 1, 0, 0, 0, 1, 0]
+    },
+    {
+        name: 'Instagram',
+        image: 'assets/instagram.png',
+        attack: 1,
+        defense: 1,
+        arrows: [0, 0, 0, 0, 1, 1, 1, 0]
+    },
+    {
+        name: 'IntelliJ',
+        image: 'assets/intellij.png',
+        attack: 3,
+        defense: 2,
+        arrows: [1, 0, 0, 0, 0, 1, 1, 0]
+    },
+    {
+        name: 'JavaZone',
+        image: 'assets/javazone.png',
+        attack: 7,
+        defense: 0,
+        arrows: [0, 0, 0, 0, 1, 0, 0, 0]
+    },
+    {
+        name: 'Jenkins',
+        image: 'assets/jenkins.png',
+        attack: 3,
+        defense: 3,
+        arrows: [0, 0, 1, 0, 1, 0, 1, 0]
+    },
+    {
+        name: 'Like',
+        image: 'assets/like.png',
+        attack: 3,
+        defense: 1,
+        arrows: [1, 1, 1, 1, 0, 1, 1, 1]
+    },
+    {
+        name: 'MySQL',
+        image: 'assets/mysql.png',
+        attack: 1,
+        defense: 4,
+        arrows: [0, 1, 0, 1, 0, 0, 1, 0]
+    },
+    {
+        name: 'PostgreSQL',
+        image: 'assets/postgresql.png',
+        attack: 0,
+        defense: 6,
+        arrows: [1, 0, 1, 0, 0, 0, 1, 0]
+    },
+    {
+        name: 'Postman',
+        image: 'assets/postman.png',
+        attack: 4,
+        defense: 1,
+        arrows: [0, 1, 0, 0, 0, 0, 0, 0]
+    },
+    {
+        name: 'Safari',
+        image: 'assets/safari.png',
+        attack: 1,
+        defense: 1,
+        arrows: [0, 1, 0, 0, 0, 1, 0, 0]
+    },
+    {
+        name: 'Scala',
+        image: 'assets/scala.png',
+        attack: 2,
+        defense: 2,
+        arrows: [1, 0, 0, 0, 1, 0, 0, 0]
+    },
+    {
+        name: 'Sublime Text',
+        image: 'assets/sublime.png',
+        attack: 1,
+        defense: 3,
+        arrows: [0, 0, 0, 1, 0, 0, 1, 0]
+    },
+    {
+        name: 'Twitter',
+        image: 'assets/twitter.png',
+        attack: 4,
+        defense: 4,
+        arrows: [1, 0, 0, 0, 0, 1, 0, 0]
     }
 ];
